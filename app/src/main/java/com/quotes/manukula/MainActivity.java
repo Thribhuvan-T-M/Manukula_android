db5af171ccba2c715d54efa01109e50ee455b99f
Thribhuvan-T-M <116435944+Thribhuvan-T-M@users.noreply.github.com>
2023-04-10 14:39:51 +0530

    on boarding

--- app/src/main/java/com/quotes/manukula/MainActivity.java
+++ app/src/main/java/com/quotes/manukula/MainActivity.java
@@ -10,40 +10,28 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.quotes.manukula.Common.OnBoardingActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageView logo;
-    private TextView text;
-    private Animation fade,popup;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logo = findViewById(R.id.logo);
-        text = findViewById(R.id.textView2);
-        fade = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        popup = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        ImageView logo = findViewById(R.id.logo);
+        TextView text = findViewById(R.id.textView2);
+        Animation fade = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation popup = AnimationUtils.loadAnimation(this, R.anim.fade_in);
 
         logo.setAnimation(fade);
         text.setAnimation(popup);
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, OnBoardingActivity.class));
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, OnBoardingActivity.class));
+            finish();
         },3000);
-
-
-
-
     }
 }
