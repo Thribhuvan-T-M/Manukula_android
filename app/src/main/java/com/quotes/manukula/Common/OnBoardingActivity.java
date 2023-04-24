112b2e3a89193599c160691c75874dcd0ed11890
Thribhuvan-T-M <116435944+Thribhuvan-T-M@users.noreply.github.com>
2023-04-24 11:55:59 +0530

    Font family , On Boarding page

--- app/src/main/java/com/quotes/manukula/Common/OnBoardingActivity.java
+++ app/src/main/java/com/quotes/manukula/Common/OnBoardingActivity.java
@@ -2,30 +2,32 @@ package com.quotes.manukula.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.quotes.manukula.Auth.LoginActivity;
 import com.quotes.manukula.R;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e("on resume","on resume");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e("on destroy","on destroy");
-    }
+    private Button onBoardingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
 
-        Log.e("on create","on create");
+        onBoardingButton = findViewById(R.id.onBoarding_Button);
+
+        onBoardingButton.setOnClickListener(view -> {
+            Intent onBoardingIntent = new Intent(OnBoardingActivity.this, LoginActivity.class);
+            startActivity(onBoardingIntent);
+            finish();
+        });
+
+
     }
 }
