4f26d66414587b228207e536fb95c6f5819a52bd
Thribhuvan-T-M <116435944+Thribhuvan-T-M@users.noreply.github.com>
2023-04-25 18:23:52 +0530

    Login Page with logics

--- app/src/main/java/com/quotes/manukula/Auth/LoginActivity.java
+++ app/src/main/java/com/quotes/manukula/Auth/LoginActivity.java
@@ -4,29 +4,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.quotes.manukula.Core.DashboardActivity;
 import com.quotes.manukula.R;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView registerButton;
-
+    private EditText emailLoginET, passwordLoginET;
+    private Button loginBT;
+    private FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        registerButton = findViewById(R.id.back_to_signup_button);
+        init();
+        logicsInMain();
+    }
 
-        registerButton.setOnClickListener(view ->{
-            Intent signUpIntent = new Intent(this,SignUpActivity.class);
+    private void init() {
+        registerButton = findViewById(R.id.back_to_signup_button);
+        auth = FirebaseAuth.getInstance();
+        emailLoginET = findViewById(R.id.email_login_et);
+        passwordLoginET = findViewById(R.id.password_login_et);
+        loginBT = findViewById(R.id.login_button);
+    }
+
+    private void logicsInMain() {
+        //Signup redirect
+        registerButton.setOnClickListener(view -> {
+            Intent signUpIntent = new Intent(this, SignUpActivity.class);
             startActivity(signUpIntent);
             finish();
 
+        });
+
+        //Login user
+        loginBT.setOnClickListener(view -> {
+            if (!TextUtils.isEmpty(emailLoginET.getText().toString())) {
+                if (!TextUtils.isEmpty(passwordLoginET.getText().toString())) {
+                    loginUserFunc(view);
+                } else {
+                    Snackbar.make(view, "Please enter your password", Snackbar.LENGTH_SHORT).show();
+                }
+            } else {
+                Snackbar.make(view, "Please enter your email id", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginUserFunc(View view) {
+        auth.signInWithEmailAndPassword(emailLoginET.getText().toString(), passwordLoginET.getText().toString()).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Intent intent = new Intent(this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Snackbar.make(view, "Login Failed", Snackbar.LENGTH_SHORT).show();
+            }
         });
     }
 }
